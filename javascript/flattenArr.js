ef973bbb14f56d1dae437fe86c4e01bacac171ea
wangli <wlkp9108@gmail.com>
2023-03-19 10:38:40 +0800

    使用【生成器函数】实现数组扁平化

--- javascript/flattenArr.js
+++ javascript/flattenArr.js
@@ -1,5 +1,7 @@
 
-// 数组扁平化
+let arr = [1, [2, 3], 4, [5, [6, 7], 8], 9];
+
+// 数组扁平化_reduce
 function flattenArr(arr) {
   if (!Array.isArray(arr)) return;
   let result = [];
@@ -8,3 +10,21 @@ function flattenArr(arr) {
   }, []);
   return result;
 }
+
+// 数组扁平化_*gen
+function flattenArr_gen(arr) {
+  function* arrGen(arr) {
+    if (Array.isArray(arr)) {
+      for (let i = 0; i < arr.length; i++) {
+        yield* arrGen(arr[i]);
+      }
+    } else {
+      yield arr;
+    }
+  }
+  return [...arrGen(arr)];
+}
+
+// test
+// console.log(flattenArr(arr));
+console.log(flattenArr_gen(arr));
